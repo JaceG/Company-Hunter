7cc345806ffc38b65082e0624192c8957d4328bf
Jace Galloway <jace.galloway@gmail.com>
2025-07-27 22:18:30 -0400

    feat: add Hotjar analytics tracking
    - Install @hotjar/browser package
    - Initialize Hotjar in main.tsx with site ID 6476685
    - Enable user behavior tracking and heatmaps for UX insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import Hotjar from '@hotjar/browser';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Initialize Hotjar
+const siteId = 6476685;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
+createRoot(document.getElementById('root')!).render(<App />);
